306b5a47f12accd02943ee861a13eebdb48268b4
Lauraellen17 <laura.ellen@gec.inatel.br>
2020-01-19 16:44:32 -0300

    first commit

---
+++ src/main/java/com/algaworks/festa/GestaoFesta5Application.java
@@ -0,0 +1,14 @@
+package com.algaworks.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFesta5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFesta5Application.class, args);
+	}
+
+}
+
